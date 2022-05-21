0d445fec1d4e43601048260e5cd85c948ca222ac
afnan-1 <roman.nadeem@gmail.com>
2022-05-21 21:58:04 +0500

    change engine value

--- core/settings.py
+++ core/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         'NAME': os.environ["DB_NAME"],
         'USER': os.environ["DB_USER"],
         'PASSWORD': os.environ["DB_PASS"],
